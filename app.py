8530d825b820a1a3a65eb839fb454a4a0987601a
AntonSangho <sanghoemail@gmail.com>
2022-03-24 17:29:10 +0900

    tweet

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,jsonify, request
 app             = Flask(__name__)
 app.users       = {}
 app.id_count    = 1
+app.tweets      = []
 
 @app.route("/ping", methods=['GET'])
 def ping():
@@ -18,3 +19,22 @@ def sign_up():
 
     return jsonfy(new_user)
 
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet   = payload['tweet']
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    if len(tweet) > 300:
+        return '300자를 초과했습니다.', 400
+
+    user_id = int(payload['id'])
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet'  : tweet
+    })
+
+    return '', 200
