92582b42eef5d0ecdb53c852e4c8eaf311362f80
AntonSangho <sanghoemail@gmail.com>
2022-03-24 17:14:38 +0900

    ping

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app             = Flask(__name__)
 app.users       = {}
 app.id_count    = 1
 
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
+
 
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
